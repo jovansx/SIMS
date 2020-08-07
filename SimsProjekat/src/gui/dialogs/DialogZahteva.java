dff5a97fd7b1ec5a84e7b8f98bdb36ca635cef2d
draganaF <filipovic.dada@gmail.com>
2020-08-07 13:25:12 +0200

    Preradjena funkcionalnost Obrada Zahteva kod administratora

--- SimsProjekat/src/gui/dialogs/DialogZahteva.java
+++ SimsProjekat/src/gui/dialogs/DialogZahteva.java
@@ -1,24 +1,69 @@
 package gui.dialogs;
 
+import gui.elementi.ElementZadatka;
+import gui.elementi.ElementZahtjeva;
 import gui.panels.PanelZahteva;
-import model.Zahtev;
+import model.*;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DialogZahteva extends JDialog {
-    public PanelZahteva panel;
+    private JPanel panel;
+    private List<ElementZahtjeva> zadaci;
+    private JButton posalji, otkazi;
+    private JPanel skrolPanel;
+    private JScrollPane skrol;
     public ArrayList<Zahtev> zahtevi;
+
 
     public DialogZahteva(ArrayList<Zahtev> zahtevi){
         this.zahtevi=zahtevi;
-        panel=new PanelZahteva(this,zahtevi);
+        zadaci=new ArrayList<>();
 
         setTitle("Obrada zahteva");
-        setSize(700, 650);
+        setSize(500, 650);
         setResizable(false);
-        setContentPane(panel);
         setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
+
+        skrol=new JScrollPane();
+        skrol.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        skrolPanel=new JPanel();
+        skrolPanel.setLayout(new BoxLayout(skrolPanel, BoxLayout.Y_AXIS));
+        ucitajSkrol();
+    }
+
+    private void ucitajSkrol() {
+        resetElemente();
+
+        for( Zahtev z: zahtevi){
+            ElementZahtjeva ez= new ElementZahtjeva(z, this);
+            zadaci.add(ez);
+            skrolPanel.add(ez);
+            JLabel labela = new JLabel("                                        ");
+            labela.setForeground(new Color(153, 204, 255));
+            skrolPanel.add(labela);
+        }
+
+        skrolPanel.validate();
+        skrolPanel.repaint();
+
+        skrol.validate();
+        skrol.repaint();
+
+        skrol.setViewportView(skrolPanel);
+        skrol.setPreferredSize(new Dimension(400, 540));
+        add(skrol,BorderLayout.CENTER);
+    }
+
+    private void resetElemente(){
+        zadaci.clear();
+        skrolPanel.removeAll();
+        skrolPanel.validate();
+        skrolPanel.repaint();
     }
 }
--- SimsProjekat/src/gui/elementi/ElementZahtjeva.java
+++ SimsProjekat/src/gui/elementi/ElementZahtjeva.java
@@ -11,7 +11,7 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class ElementZahtjeva extends JPanel {
-    private JLabel korisnik, naziv, opis, recenzija, muzickaDela,id, ocena, komentar;
+    private JLabel korisnik, naziv, opis, recenzija, muzickaDela,id, ocena, komentar, korisnik1;
     private JButton odobri, odbij;
     private Zahtev zahtev;
     private DialogZahteva dialog;
@@ -21,55 +21,70 @@ public class ElementZahtjeva extends JPanel {
         this.zahtev=z;
         setLayout(new BorderLayout(0, 0));
         setBackground(new Color(229, 255, 204));
-        setPreferredSize(new Dimension(400, 220));
+        setPreferredSize(new Dimension(400, 340));
         setLayout(null);
         namesti();
     }
 
     private void namesti() {
-        korisnik=new JLabel("Korisnik: "+zahtev.getPodnosilacZahteva().getIme()+" "+
-                zahtev.getPodnosilacZahteva().getPrezime()+" "+zahtev.getPodnosilacZahteva().getNalog().getKorisnickoIme());
-        korisnik.setBounds(20, 20, 300, 30);
+        korisnik=new JLabel("Korisnik: ");
+        korisnik.setBounds(20, 20, 70, 30);
+        korisnik.setFont(new Font("Courier", Font.BOLD,14));
         add(korisnik);
+        korisnik1= new JLabel(zahtev.getPodnosilacZahteva().getIme()+" "+
+                zahtev.getPodnosilacZahteva().getPrezime()+" "+zahtev.getPodnosilacZahteva().getNalog().getKorisnickoIme());
+        korisnik1.setBounds(90, 20, 200, 30);
+        add(korisnik1);
 
-        naziv= new JLabel("Naziv: "+zahtev.getNaziv());
-        naziv.setBounds(20, 60, 300, 30);
+        naziv= new JLabel("Naziv: ");
+        naziv.setFont(new Font("Courier", Font.BOLD,12));
+        naziv.setBounds(20, 60, 70, 20);
         add(naziv);
 
-        opis=new JLabel("Opis: "+zahtev.getOpis());
-        opis.setBounds(20, 100, 300, 30);
+        JLabel naziv1=new JLabel(zahtev.getNaziv());
+        naziv1.setBounds(90, 60, 200, 20);
+        add(naziv1);
+
+        opis=new JLabel("Opis: ");
+        opis.setBounds(20, 90, 70, 20);
+        opis.setFont(new Font("Courier", Font.BOLD,12));
         add(opis);
 
+        JLabel opis1= new JLabel(zahtev.getOpis());
+        opis1.setBounds(90, 90, 400, 20);
+        add(opis1);
+
         recenzija=new JLabel("Recenzija: ");
-        recenzija.setBounds(20, 130, 40, 30);
+        recenzija.setFont(new Font("Courier", Font.BOLD,12));
+        recenzija.setBounds(20, 120, 100, 20);
         add(recenzija);
 
         id=new JLabel("    id: "+zahtev.getRecenzija().getId());
-        id.setBounds(20, 150, 100, 30);
+        id.setBounds(20, 160, 100, 20);
         add(id);
 
         ocena=new JLabel("    ocena: "+zahtev.getRecenzija().getOcena());
-        ocena.setBounds(20, 180, 100, 30);
+        ocena.setBounds(20, 180, 100, 20);
         add(ocena);
 
         komentar=new JLabel("    komentar: "+zahtev.getRecenzija().getKomentar());
-        komentar.setBounds(20, 210, 300, 30);
+        komentar.setBounds(20, 200, 300, 20);
         add(komentar);
 
         muzickaDela=new JLabel("    muzicko delo/a: "+ naziviMuzickihDela());
-        muzickaDela.setBounds(20, 240, 400, 30);
+        muzickaDela.setBounds(20, 220, 400, 20);
         add(muzickaDela);
 
         odobri=new JButton("Odobri");
-        odobri.setBounds(300, 300, 80, 30);
+        odobri.setBounds(270, 280, 80, 30);
         odobri.setBackground(new Color(0, 77, 102));
         odobri.setForeground(Color.white);
         add(odobri);
 
         odbij= new JButton("Odbij");
-        odbij.setBounds(390,300, 80, 30);
+        odbij.setBounds(360,280, 80, 30);
         odbij.setBackground(new Color(0, 77, 102));
-        odbij.setBackground(Color.white);
+        odbij.setForeground(Color.white);
         add(odbij);
 
     }
