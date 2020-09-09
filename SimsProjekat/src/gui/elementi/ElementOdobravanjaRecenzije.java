bbfff12ce9d3744150fbf36b80ac4ba542670f69
draganaF <filipovic.dada@gmail.com>
2020-09-09 11:51:26 +0200

    Neke sitnice oko koda su ispravljene sto se tice funkcionalnosti admina

--- SimsProjekat/src/gui/elementi/ElementOdobravanjaRecenzije.java
+++ SimsProjekat/src/gui/elementi/ElementOdobravanjaRecenzije.java
@@ -1,7 +1,9 @@
 package gui.elementi;
 
+import dao.MuzickoDeloDAO;
 import dao.RecenzijaDAO;
 import gui.dialogs.DialogAdminovihRecenzija;
+import model.MuzickoDelo;
 import model.Recenzija;
 
 import javax.swing.*;
@@ -10,11 +12,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ElementOdobravanjaRecenzije extends JPanel {
     private DialogAdminovihRecenzija dialog;
     private Recenzija recenzija;
-    private JLabel id, ocjena, komentar, autor, id1, ocjena1, komentar1, autor1;
+    private JLabel id, ocjena, komentar, autor, id1, ocjena1, komentar1, autor1, djelo, djelo1;
     private JButton odobri, odbij;
     
     public ElementOdobravanjaRecenzije(DialogAdminovihRecenzija d, Recenzija r){
@@ -26,8 +29,10 @@ public class ElementOdobravanjaRecenzije extends JPanel {
         setLayout(null);
         setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
         namesti();
-        
+        podesiAkcije();
     }
+
+
 
     private void namesti() {
         id=new JLabel("Id recenzije: ");
@@ -66,40 +71,72 @@ public class ElementOdobravanjaRecenzije extends JPanel {
         autor1.setBounds(100, 80, 200, 20);
         add(autor1);
 
+        djelo=new JLabel("Muzicko delo: ");
+        djelo.setBounds(20,100, 100, 20);
+        djelo.setFont(new Font("Courier", Font.BOLD, 12));
+        add(djelo);
+
+        String nazivDjela="";
+        if(recenzija.getMuzickoDelo()==null){
+            nazivDjela=getNazivDjela((ArrayList<MuzickoDelo>) MuzickoDeloDAO.getMuzickaDelaIzvodjenja(recenzija.getIzvodnjenje().getId()));
+        }
+        else{
+            nazivDjela=recenzija.getMuzickoDelo().getNazivDela();
+        }
+        djelo1=new JLabel(nazivDjela);
+        djelo1.setBounds(120, 100, 200, 20);
+        add(djelo1);
+
         odobri=new JButton("Odobri");
         odobri.setBackground(new Color(77, 121, 255));
         odobri.setForeground(Color.white);
         odobri.setBounds(270, 120, 80, 30);
-        odobri.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                recenzija.setOdobreno(true);
-                try {
-                    RecenzijaDAO.update(recenzija);
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-            }
-        });
         add(odobri);
 
         odbij= new JButton("Odbij");
         odbij.setBounds(360,120, 80, 30);
         odbij.setBackground(new Color(77, 121, 255));
         odbij.setForeground(Color.white);
+        add(odbij);
+
+    }
+    private void podesiAkcije() {
+        odobri.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                recenzija.setOdobreno(true);
+                try {
+                    RecenzijaDAO.update(recenzija);
+                    JOptionPane.showMessageDialog(null, "Rezenzija je odobrena.");
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+                dialog.deleteComponent(ElementOdobravanjaRecenzije.this);
+            }
+        });
+
         odbij.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 recenzija.setOdobreno(false);
                 try {
                     RecenzijaDAO.update(recenzija);
+                    JOptionPane.showMessageDialog(null, "Recenzija je odbijena.");
                 } catch (SQLException throwables) {
                     throwables.printStackTrace();
                 }
+                dialog.deleteComponent(ElementOdobravanjaRecenzije.this);
             }
         });
-        add(odbij);
 
+    }
+
+    public static String getNazivDjela(ArrayList<MuzickoDelo> djela){
+        String naziv="";
+        for(MuzickoDelo d: djela){
+            naziv+=d.getNazivDela()+" ";
+        }
+        return naziv;
     }
 
 }
